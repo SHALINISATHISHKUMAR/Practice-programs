8a9cd66f7c03bf465e99a6853a11d3c1de7c8b36
SHALINISATHISHKUMAR <92022804+SHALINISATHISHKUMAR@users.noreply.github.com>
2021-10-07 09:28:19 -0700

    Add files via upload

---
+++ Toggle String.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main()
+{
+    
+            char s[100];
+   
+            int i=0;
+    
+            printf("Enter string:");
+  
+            gets(s);
+    
+            while(s[i])
+
+            {
+        
+             if(s>='65'&&s<='90')
+         
+             s=s+32;
+        
+             if(s1>='97'&&s<='122')
+          
+             s=s-32
+         
+             s[i++];
+   
+            }
+   
+         printf("Toggle string is %s",s);
+   
+         return 0;
+
+}
