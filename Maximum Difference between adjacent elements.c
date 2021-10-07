70140ecaf55f63b28b8818018544d63883007c37
SHALINISATHISHKUMAR <92022804+SHALINISATHISHKUMAR@users.noreply.github.com>
2021-10-07 09:30:53 -0700

    Create Maximum Difference between adjacent elements.c

---
+++ Maximum Difference between adjacent elements.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+   int a[10],i,n,max=0,Diff;
+   printf("Size the list:");
+   scanf("%d",&n);
+   printf("Enter elements in the list:\n");
+   for(i=0;i<n;i++)
+   scanf("%d",&a[i]);
+   for(i=0;i<(n-1);i++)
+   { 
+    Diff=a[i]-a[i+1];
+    if(Diff>max)
+     max=Diff;
+   }
+   printf("Maximum difference b/w two elements:%d ",max);
+
+    return 0;
+}
+
